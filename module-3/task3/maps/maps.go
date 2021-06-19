ae0343f2e5c66d544c0a6a772335b42fd9e1fbed
Piotr Szwarc <quarcu@gmail.com>
2021-06-19 11:51:49 +0200

    Module 3 task 3: implement SortedValues

--- module-3/task3/maps/maps.go
+++ module-3/task3/maps/maps.go
@@ -1,4 +1,22 @@
 package maps
 
+import (
+	"sort"
+	"strings"
+)
+
 func SortedValues(m map[int]string) string {
+	keys := make([]int, 0, len(m))
+	result := make([]string, 0, len(m))
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	for _, v := range keys {
+		result = append(result, m[v])
+	}
+
+	return strings.Join(result, "")
 }
