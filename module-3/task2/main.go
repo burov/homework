f81783de170542704eebcc8252d44c3847c23919
Michal Kupisinski <Michal_Kupisinski@epam.com>
2021-07-14 14:38:57 +0200

    Task 2 for module 3

---
+++ module-3/task2/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello task 2 of module 3")
+}
--- module-3/task2/slices/slice.go
+++ module-3/task2/slices/slice.go
@@ -1,10 +1,17 @@
 package slices
 
+import (
+	"sort"
+)
+
 func SortStringsInPlace(s []string) {
+	sort.Strings(s)
 }
 
 func SortStringsPure(s []string) []string {
-
+	copy := s
+	sort.Strings(copy)
+	return copy
 }
 
 type User struct {
@@ -12,10 +19,29 @@ type User struct {
 	LastName  string
 }
 
+type UserList []User
+
+func (u UserList) Len() int {
+	return len(u)
+}
+
+func (u UserList) Less(i, j int) bool {
+	return u[i].FirstName < u[j].FirstName || (u[i].FirstName == u[j].FirstName && u[i].LastName < u[j].LastName)
+
+}
+
+func (u UserList) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
 func SortUsersPure(s []User) []User {
-	return r
+	var userList UserList = s
+	sort.Sort(userList)
+	return userList
 }
 
 func SortUsersPureDesc(s []User) []User {
-	return r
+	var userList UserList = s
+	sort.Sort(sort.Reverse(userList))
+	return userList
 }
