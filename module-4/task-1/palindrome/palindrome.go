90737faffdb5d90fb2a3c2fab8b3ecc8cff8d233
Anton Ilchenko <anton_ilchenko@epam.com>
2021-07-14 15:33:26 +0200

    FEAT: Module4.Task1 solved

--- module-4/task-1/palindrome/palindrome.go
+++ module-4/task-1/palindrome/palindrome.go
@@ -1,5 +1,16 @@
 package palindrome
 
 func IsPalindrome(s string) bool {
-	return true
+	return s == reverse(s)
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	size := len(runes)
+	out := make([]rune, 0)
+
+	for i := range runes {
+		out = append(out, runes[size-i-1])
+	}
+	return string(out)
 }
